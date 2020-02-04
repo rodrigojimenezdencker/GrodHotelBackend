eff46a00e825ca812d6e38425c8d645fea565afd
Gramoneda <guillemramonedabejar@gmail.com>
2020-02-04 19:15:31 +0100

    Added dashboard for back-office

---
+++ GrodHotelBackend/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrodHotelBackend.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Dashboard";
+            ViewBag.PageName = "dashboard";
+            return View();
+        }
+    }
+}
