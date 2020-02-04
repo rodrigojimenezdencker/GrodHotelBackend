e30119ecee5cd6bb5db6812bc56fc8d189413402
Gramoneda <guillemramonedabejar@gmail.com>
2020-02-04 19:16:42 +0100

    Added BO for all models

---
+++ GrodHotelBackend/Models/BO/BOHotelsChain.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrodHotelBackend.Models.BO
+{
+    public class BOHotelsChain
+    {
+    }
+}
