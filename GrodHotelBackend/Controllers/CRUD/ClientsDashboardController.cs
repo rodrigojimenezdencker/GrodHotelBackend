23c01f246345d1667906d4b1537edcdb475a77dd
Gramoneda <guillemramonedabejar@gmail.com>
2020-02-10 17:07:49 +0100

    Deleted subscribed from create user

--- GrodHotelBackend/Controllers/CRUD/ClientsDashboardController.cs
+++ GrodHotelBackend/Controllers/CRUD/ClientsDashboardController.cs
@@ -53,7 +53,7 @@ namespace GrodHotelBackend.Controllers.CRUD
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Surname,BirthDate,Dni,Email,Subscribed,Comments")] Clients clients)
+        public async Task<IActionResult> Create([Bind("Id,Name,Surname,BirthDate,Dni,Email,Comments")] Clients clients)
         {
             if (ModelState.IsValid)
             {
