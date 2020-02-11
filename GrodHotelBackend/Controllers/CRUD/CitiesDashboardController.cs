1156d38b10799bc83044cbe639c2864f7f672b7d
Rodrigo <rodrigo.jimenez.developer@gmail.com>
2020-02-11 17:18:40 +0100

    Added Cities and Countries Controller

---
+++ GrodHotelBackend/Controllers/CRUD/CitiesDashboardController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GrodHotelBackend.Models;
+
+namespace GrodHotelBackend.Controllers.CRUD
+{
+    public class CitiesDashboardController : Controller
+    {
+        private readonly Context _context;
+
+        public CitiesDashboardController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: CitiesDashboard
+        public async Task<IActionResult> Index()
+        {
+            var context = _context.Cities.Include(c => c.Countries);
+            return View(await context.ToListAsync());
+        }
+
+        // GET: CitiesDashboard/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cities = await _context.Cities
+                .Include(c => c.Countries)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cities == null)
+            {
+                return NotFound();
+            }
+
+            return View(cities);
+        }
+
+        // GET: CitiesDashboard/Create
+        public IActionResult Create()
+        {
+            ViewData["CountriesId"] = new SelectList(_context.Countries, "Id", "Name");
+            return View();
+        }
+
+        // POST: CitiesDashboard/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CountriesId,Name")] Cities cities)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cities);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountriesId"] = new SelectList(_context.Countries, "Id", "Name", cities.CountriesId);
+            return View(cities);
+        }
+
+        // GET: CitiesDashboard/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cities = await _context.Cities.FindAsync(id);
+            if (cities == null)
+            {
+                return NotFound();
+            }
+            ViewData["CountriesId"] = new SelectList(_context.Countries, "Id", "Name", cities.CountriesId);
+            return View(cities);
+        }
+
+        // POST: CitiesDashboard/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CountriesId,Name")] Cities cities)
+        {
+            if (id != cities.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cities);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CitiesExists(cities.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountriesId"] = new SelectList(_context.Countries, "Id", "Name", cities.CountriesId);
+            return View(cities);
+        }
+
+        // GET: CitiesDashboard/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cities = await _context.Cities
+                .Include(c => c.Countries)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cities == null)
+            {
+                return NotFound();
+            }
+
+            return View(cities);
+        }
+
+        // POST: CitiesDashboard/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cities = await _context.Cities.FindAsync(id);
+            _context.Cities.Remove(cities);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CitiesExists(int id)
+        {
+            return _context.Cities.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ GrodHotelBackend/Controllers/CRUD/CountriesDashboardController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GrodHotelBackend.Models;
+
+namespace GrodHotelBackend.Controllers.CRUD
+{
+    public class CountriesDashboardController : Controller
+    {
+        private readonly Context _context;
+
+        public CountriesDashboardController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: CountriesDashboard
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Countries.ToListAsync());
+        }
+
+        // GET: CountriesDashboard/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var countries = await _context.Countries
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (countries == null)
+            {
+                return NotFound();
+            }
+
+            return View(countries);
+        }
+
+        // GET: CountriesDashboard/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CountriesDashboard/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Countries countries)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(countries);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(countries);
+        }
+
+        // GET: CountriesDashboard/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var countries = await _context.Countries.FindAsync(id);
+            if (countries == null)
+            {
+                return NotFound();
+            }
+            return View(countries);
+        }
+
+        // POST: CountriesDashboard/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Countries countries)
+        {
+            if (id != countries.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(countries);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CountriesExists(countries.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(countries);
+        }
+
+        // GET: CountriesDashboard/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var countries = await _context.Countries
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (countries == null)
+            {
+                return NotFound();
+            }
+
+            return View(countries);
+        }
+
+        // POST: CountriesDashboard/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var countries = await _context.Countries.FindAsync(id);
+            _context.Countries.Remove(countries);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CountriesExists(int id)
+        {
+            return _context.Countries.Any(e => e.Id == id);
+        }
+    }
+}
