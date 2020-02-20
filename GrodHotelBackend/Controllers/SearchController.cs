483316db353b46f9d15d1fd0b13a20010eeddd58
Rodrigo <rodrigo.jimenez.developer@gmail.com>
2020-02-20 18:12:55 +0100

    Tests of form

--- GrodHotelBackend/Controllers/SearchController.cs
+++ GrodHotelBackend/Controllers/SearchController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNetCore.Mvc;
+using GrodHotelBackend.Models;
 
 namespace GrodHotelBackend.Controllers
 {
     public class SearchController : Controller
     {
-        // GET: Search
-        public ActionResult Index()
+        private readonly Context _context;
+
+        public SearchController(Context context)
         {
-            ViewBag.Title = "Search";
+            _context = context;
+        }
+        // GET: Search
+        public ActionResult Index(string hotelName)
+        {
+            ViewBag.Title = hotelName;
             ViewBag.PageName = "search";
-            return View();
+            IList<Hotels> hotel = _context.Hotels.Where(el => el.Availability == true).ToList();
+            return View(hotel);
         }
     }
 }
