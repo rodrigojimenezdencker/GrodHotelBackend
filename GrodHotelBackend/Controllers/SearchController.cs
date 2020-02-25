f494b54c5d204ce0ef960c0092b82fb2b21961ef
Gramoneda <guillemramonedabejar@gmail.com>
2020-02-25 16:08:27 +0100

    Search done

--- GrodHotelBackend/Controllers/SearchController.cs
+++ GrodHotelBackend/Controllers/SearchController.cs
@@ -15,13 +15,34 @@ namespace GrodHotelBackend.Controllers
         {
             _context = context;
         }
+
+        public class ViewModel
+        {
+            public IEnumerable<Hotels> Hotels { get; set; }
+            public IEnumerable<Cities> Cities { get; set; }
+        }
+
+        private List<Hotels> GetHotels()
+        {
+            List<Hotels> hotels = _context.Hotels.ToList();
+            return hotels;
+        }
+
+        private List<Cities> GetCities()
+        {
+            List<Cities> cities = _context.Cities.ToList();
+            return cities;
+        }
+
         // GET: Search
         public ActionResult Index(string hotelName)
         {
             ViewBag.Title = hotelName;
-            ViewBag.PageName = "search";
-            IList<Hotels> hotel = _context.Hotels.Where(el => el.Availability == true).ToList();
-            return View(hotel);
+            ViewBag.PageName = "search"; 
+            ViewModel mymodel = new ViewModel();
+            mymodel.Hotels = GetHotels();
+            mymodel.Cities = GetCities();
+            return View(mymodel);
         }
     }
 }
