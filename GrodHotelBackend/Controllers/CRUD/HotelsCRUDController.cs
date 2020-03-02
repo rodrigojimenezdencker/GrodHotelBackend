5bccbf3a3eb14d727b507040d144f33a2d8577d0
Rodrigo <rodrigo.jimenez.developer@gmail.com>
2020-03-02 10:16:25 +0100

    Re-done crud pages for Hotels and Rooms

--- GrodHotelBackend/Controllers/CRUD/HotelsCRUDController.cs
+++ GrodHotelBackend/Controllers/CRUD/HotelsCRUDController.cs
@@ -58,7 +58,7 @@ namespace GrodHotelBackend.Controllers.CRUD
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,HotelsChainId,CitiesId,Name,Description,Trending,Availability,Image,SmallImage")] Hotels hotels)
+        public async Task<IActionResult> Create([Bind("Id,HotelsChainId,CitiesId,Name,Description,Trending,Availability,Image,SmallImage,Slug")] Hotels hotels)
         {
             if (ModelState.IsValid)
             {
@@ -94,7 +94,7 @@ namespace GrodHotelBackend.Controllers.CRUD
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,HotelsChainId,CitiesId,Name,Description,Trending,Availability,Image,SmallImage")] Hotels hotels)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,HotelsChainId,CitiesId,Name,Description,Trending,Availability,Image,SmallImage,Slug")] Hotels hotels)
         {
             if (id != hotels.Id)
             {
