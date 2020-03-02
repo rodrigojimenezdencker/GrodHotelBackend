088db4c4b66520df8b1c0b38c2f487d4bc3e2908
Rodrigo <rodrigo.jimenez.developer@gmail.com>
2020-03-02 10:17:54 +0100

    Made changes on bookings Model

---
+++ GrodHotelBackend/Migrations/20200228173451_Modified RoomsId on Bookings.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GrodHotelBackend.Migrations
+{
+    public partial class ModifiedRoomsIdonBookings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Rooms_RoomsId",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoomsId",
+                table: "Bookings",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Rooms_RoomsId",
+                table: "Bookings",
+                column: "RoomsId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Rooms_RoomsId",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoomsId",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Rooms_RoomsId",
+                table: "Bookings",
+                column: "RoomsId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
