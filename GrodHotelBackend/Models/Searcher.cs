fe387ea28d4af521703d038fefb3ce2860520b89
Rodrigo <rodrigo.jimenez.developer@gmail.com>
2020-03-04 10:12:46 +0100

    Cleaned up Searcher class

--- GrodHotelBackend/Models/Searcher.cs
+++ GrodHotelBackend/Models/Searcher.cs
@@ -23,19 +23,6 @@ namespace GrodHotelBackend.Models
                     booking.EntryDate < filters.EntryDate &&
                     filters.EntryDate > booking.LeavingDate));
 
-
-            //var RoomsGeneralQuery = _context.Bookings
-            //    .Include(booking => booking.Rooms)
-            //    .ThenInclude(room => room.Hotels)
-            //    .Where(booking => (filters.EntryDate < booking.EntryDate &&
-            //    booking.EntryDate > filters.LeavingDate) ||
-            //    (booking.EntryDate < filters.EntryDate &&
-            //    filters.EntryDate > booking.LeavingDate))
-            //    .Select(booking => booking.Rooms);
-
-            //List<Rooms> RoomsWithoutBookings = new List<Rooms>();
-            //List<Rooms> RoomsWithPrice = new List<Rooms>();
-
             if (filters.MinimumPrice.HasValue)
             {
                 RoomsGeneralQuery = RoomsGeneralQuery.Where(room => room.Price < filters.MaximumPrice && room.Price > filters.MinimumPrice);
@@ -44,16 +31,4 @@ namespace GrodHotelBackend.Models
             return RoomsGeneralQuery.Where(x => x.Hotels.CitiesId == filters.City).ToList();
         }
     }
-
-    /*public static class BookingExtension
-    {
-        public static bool Intersects(Bookings booking, Filters filters)
-        {
-            if (filters.EntryDate > booking.EntryDate && booking.EntryDate < filters.LeavingDate)
-                return true;
-            else if (booking.EntryDate > filters.EntryDate && filters.EntryDate < booking.EntryDate)
-                return true;
-            else return false;
-        }
-    }*/
 }
