1aae5cb965ebc97444009595787a7b7a07cff640
Rodrigo <rodrigo.jimenez.developer@gmail.com>
2020-03-09 02:26:55 +0100

    Check if widget is null

--- GrodHotelBackend/wwwroot/Assets/page/room/js/script.js
+++ GrodHotelBackend/wwwroot/Assets/page/room/js/script.js
@@ -1,14 +1,17 @@
 setuppers['room'] = function () {
     console.log('Room');
+
     var widget = document.querySelector('[data-widget="room_form"]');
-    widget.addEventListener("submit", function (event) {
-        event.preventDefault();
-        if (validateEntryDate(widget.querySelector('[data-hook="entry_date"]').value)
-            || validateLeavingDate(widget.querySelector('[data-hook="leaving_date"]').value)
-            || validateNumberAdults(parseInt(widget.querySelector('[data-hook="numberAdults"]').value))
-            || validateNumberMinors(parseInt(widget.querySelector('[data-hook="numberMinors"]').value))) {
-            return;
-        }
-        widget.submit();
-    });
+    if (widget != null) {
+        widget.addEventListener("submit", function (event) {
+            event.preventDefault();
+            if (validateEntryDate(widget.querySelector('[data-hook="entry_date"]').value)
+                || validateLeavingDate(widget.querySelector('[data-hook="leaving_date"]').value)
+                || validateNumberAdults(parseInt(widget.querySelector('[data-hook="numberAdults"]').value))
+                || validateNumberMinors(parseInt(widget.querySelector('[data-hook="numberMinors"]').value))) {
+                return;
+            }
+            widget.submit();
+        });
+    }
 }
